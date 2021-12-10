9d6990cef6b61aeb31a83d7fb83a4f598d428da8
CardaCardz <CardaCardz@protonmail.com>
2021-12-10 18:29:10 +0200

    Revert "Configuration Reader implementation."
    This reverts commit 378d496e992ec09a7178a6a5f6540f7915df503b.

--- src/main/java/TokenDistributor.java
+++
@@ -1,9 +0,0 @@
-import configuration.ConfigurationReader;
-import configuration.ConfigurationReaderIT;
-
-public class TokenDistributor {
-    public static void main(String[] args) {
-        ConfigurationReaderIT readerIT = new ConfigurationReader();
-        System.out.println(readerIT.readSingleProperty("dbs"));
-    }
-}
